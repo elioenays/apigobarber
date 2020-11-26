d4e06052f1f4b4f37aa49d1a1eec17c21c221612
ELioenay <elionay407@gmail.com>
2020-11-25 21:46:08 -0300

    Dependence Inversion Principle

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,29 +1,31 @@
-import { Router } from "express";
-import { parseISO } from "date-fns";
-import { getCustomRepository } from "typeorm";
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
 
-import AppointmentRepository from "@modules/appointments/infra/typeorm/repositories/AppointmentsRepository";
-import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
-import ensureAuthenticated from "@modules/users/infra/middlewares/ensureAuthenticated";
+import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+import ensureAuthenticated from '@modules/users/infra/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
+const appointmentsRepository = new AppointmentRepository();
+
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get("/", async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  const appointments = await appointmentsRepository.find();
+//   return response.json(appointments);
+// });
 
-  return response.json(appointments);
-});
-
-appointmentsRouter.post("/", async (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
