2b9664d26f31dec178ffd9d34fd1e18948c1005c
Elioenay Silva <elionay407@gmail.com>
2020-12-07 22:16:57 -0300

    Atualização do perfil

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,27 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfile {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, name, email }: IRequest): Promise<void> {}
+}
+
+export default UpdateProfile;
