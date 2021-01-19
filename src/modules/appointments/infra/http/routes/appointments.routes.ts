58b0410131ea0fd35a0c36768eab2dd45d44dada
Elioenay <Elionay407@gmail.com>
2021-01-19 13:37:09 -0300

    Agenda do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/middlewares/ensureAuthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsCotroller';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
