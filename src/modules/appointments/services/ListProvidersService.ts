e22bda6f9cce422cd61036b54bf2569a4a7ec089
Elioenay Silva <elionay407@gmail.com>
2021-03-07 16:35:01 -0300

    Cache Lista de Providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/Models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -12,12 +13,23 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private CacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.CacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+    }
+
+    await this.CacheProvider.save(`providers-list:${user_id}`, users);
 
     return users;
   }
--- src/shared/container/providers/CacheProvider/Implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/Implementations/RedisCacheProvider.ts
@@ -9,12 +9,20 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cache.config.redis);
   }
 
-  public async save(key: string, value: string): Promise<void> {
+  public async save(key: string, value: ay): Promise<void> {
     await this.client.set(key, JSON.stringify(value));
   }
 
-  public async recover(key: string): Promise<void> {
+  public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
   }
 
   public async invalidate(key: string): Promise<void>;
